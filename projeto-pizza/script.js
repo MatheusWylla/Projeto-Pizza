a619b33bd84421953454853b5cf5e10c3f77f05f
Matheus Wyllamy <111541044+MatheusWylla@users.noreply.github.com>
2022-12-01 13:28:47 -0300

    modal finalizado

--- projeto-pizza/script.js
+++ projeto-pizza/script.js
@@ -2,7 +2,7 @@ let modalQt = 1;
 const c = (el) => document.querySelector(el)
 const cs = (el) => document.querySelectorAll(el)
 
-
+// listagem das pizza
 pizzaJson.map((item, index) => {
     let pizzaItem = c('.models .pizza-item').cloneNode(true)
 
@@ -40,4 +40,35 @@ pizzaJson.map((item, index) => {
     })
 
     c('.pizza-area').append(pizzaItem)
+})
+
+// eventos do MODAL
+function closeModal() {
+    c('.pizzaWindowArea').style.opacity = 0
+    setTimeout(() => {
+        c('.pizzaWindowArea').style.display = 'none'
+    }, 500)
+}
+
+cs('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item) => {
+    item.addEventListener('click', closeModal)
+})
+
+c('.pizzaInfo--qtmenos').addEventListener('click', () => {
+    if (modalQt > 1) {
+        modalQt--;
+        c('.pizzaInfo--qt').innerHTML = modalQt;
+    }
+})
+
+c('.pizzaInfo--qtmais').addEventListener('click', () => {
+    modalQt++
+    c('.pizzaInfo--qt').innerHTML = modalQt;
+
+})
+cs('.pizzaInfo--size').forEach((size, sizeIndex) => {
+    size.addEventListener('click', (e) => {
+        c('.pizzaInfo--size.selected').classList.remove('selected')
+        size.classList.add('selected')
+    })
 })
